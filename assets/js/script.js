83f3708007c3dbeb6db90bdd02725a3a72a0d0ba
Jack Atkerson <jatkerson18@gmail.com>
2022-05-22 17:21:43 -0500

    created text box divs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
+var textHourEl = 9;
+var textSuffixEl = ":00am";
 
 var displayDate = function() {
-    var date = moment().format('dddd MMMM Do YYYY')
+    var date = moment().format('dddd, MMMM Do, YYYY')
     $('#counter').html(date);
 };
 
+var setColor = function() {
+    
+};
 displayDate();
